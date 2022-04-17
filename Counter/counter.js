3954b4ed21f7fea295119c704cc0b2d85f204552
lucasplr <lucasriffel55@gmail.com>
2022-04-17 20:55:36 -0300

    update

--- Counter/counter.js
+++ Counter/counter.js
@@ -8,8 +8,20 @@ btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
         const styles = e.currentTarget.classList
         if (styles.contains("decrease")){
-            cont--
+            count--
+        }else if (styles.contains("increase")){
+            count++
+        }else{
+            count = 0
         }
         counter.textContent = count
+        if(count > 0){
+            counter.style.color = "green"
+        }else if (count < 0){
+            counter.style.color = "red"
+        }else{
+            counter.style.color = "black"
+        }
     })
+    
 })
