3b8cd74e4221b932558cef122e67096d039fe741
lucasplr <lucasriffel55@gmail.com>
2022-04-17 19:11:52 -0300

    update

--- Counter/counter.js
+++ Counter/counter.js
@@ -1,32 +1,28 @@
-const counter = document.getElementById("register")
-
+const counter = document.querySelector(".register")
 const btnres = document.getElementById("btn2")
 const btninc = document.getElementById("btn")
 const btndec = document.getElementById("btn3")
 
-
+let cont = 0
 
 btn.addEventListener('click', function(){
-
-
-    document.body.style.backgroundColor = colors[randomNumber]
-    color.textContent = colors[randomNumber]
+    cont = cont - 1 
+    counter.textContent = cont
+    return cont
     })
 
 btn2.addEventListener('click', function(){
     // get random number between 0 - 3[0]
-        const randomNumber = getRandomNumber()
-        console.log(randomNumber)
-        document.body.style.backgroundColor = colors[randomNumber]
-        color.textContent = colors[randomNumber]
+        cont = 0
+        counter.textContent = cont
+        return cont
     })
 
 btn3.addEventListener('click', function(){
         // get random number between 0 - 3[0]
-            const randomNumber = getRandomNumber()
-            console.log(randomNumber)
-            document.body.style.backgroundColor = colors[randomNumber]
-            color.textContent = colors[randomNumber]
+            cont = cont + 1
+            counter.textContent = cont
+            return cont
      })
 
 
