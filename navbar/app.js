cb49de702321ff43c26154d403090ebfb8d8eda2
lucasplr <lucasriffel55@gmail.com>
2022-04-19 17:31:44 -0300

    update

---
+++ navbar/app.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector(".links")
+let menhid = document.querySelector(".show-links")
+let btn = document.querySelector(".nav-toggle")
+
+btn.addEventListener("click", function(){
+   // if(menu.classList.contains("show-links")){
+  //     menu.classList.remove("show-links")
+   // }else{
+  //      menu.classList.add("show-links")
+  //  }
+    menu.classList.toggle("show-links")
+    })
+    
+    
+
+
+
+
+
+
+    // classList - show/gets all classes
+    // contains - checks classList for specific class
+    //add - add class
+    //remove - remove class
+    // toggle - toggle class
