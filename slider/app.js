d6e3781d1cd5a557651cc5cc63a3cd61fa92f0e5
lucasplr <lucasriffel55@gmail.com>
2022-04-25 17:44:45 -0300

    update

---
+++ slider/app.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll(".slide")
+const nextBtn = document.querySelector(".nextBtn")
+const prevBtn = document.querySelector(".prevBtn") 
+
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`
+})
+let counter = 0
+
+nextBtn.addEventListener("click",function(){
+    counter++
+    carousel()
+})
+
+prevBtn.addEventListener("click", function(){
+    counter--
+    carousel()
+}) 
+
+function carousel(){
+     slides.forEach(function(slide){
+         slide.style.transform = `transladeX(-${counter * 100}%)`
+     })
+}
