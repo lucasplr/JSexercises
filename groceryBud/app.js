e8c0d0299a6ef98d308efc3a74b9cec2a83c37de
lucasplr <lucasriffel55@gmail.com>
2022-04-24 21:06:58 -0300

    update

--- groceryBud/app.js
+++ groceryBud/app.js
@@ -24,9 +24,63 @@ function addItem(e){
     e.preventDefault()
     const value = grocery.value
     const id = new Date().getTime().toString() //In this case, this is used to every item have a unique ID, and is not the correct way to do this, is made in this way just to practice.
-    console.log(id)
+    if(value && !editFlag){
+        const element = document.createElement("article")
+        //add class
+        element.classList.add("grocery-item")
+        //add id
+        const attr = document.createAttribute("data-id")
+        attr.value = id
+        element.setAttributeNode(attr)
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button class="edit-btn" type="button">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button class="delete-btn" type="button">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`
+        // append child
+        list.appendChild(element)
+        // display alert
+        displayAlert("add item to the list", "success")
+        //show container
+        container.classList.add("show-container")
+        // add to local storage
+        addToLocalStorage(id, value)
+        // set back to default
+        setBackToDefault()
+    }
+    else if (value  && editFlag){
+        console.log("editing")
+    }
+    else{
+        displayAlert("please enter value", "danger")
+        
+    }
 
 }
-//LOCAL STORAGE
+//display alert
+function displayAlert(text, action){
+    alert.textContent = text
+    alert.classList.add(`alert-${action}`)
+    //remove alert
+    setTimeout(() => {
+        alert.textContent = ""
+        alert.classList.remove(`alert-${action}`)
+    }, 1000);
+}
+// set back to default
+ function setBackToDefault(){
+     grocery.value = ""
+     editFlag = false
+     editID = ""
+     submitBtn.textContent = "submit" // for the basic scenario, only is needed the first condition, but, this function will be executed in diferent elements.
 
+ }
+//LOCAL STORAGE
+function addToLocalStorage(id, value){
+    console.log("added to local storage")
+}
 //SETUP ITEMS
